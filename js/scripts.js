97c87cc19cd48a5df14fd31d023d393b4855f862
Wilfred Mallawa <wilfred.mallawa@wdc.com>
2022-05-03 12:12:31 +1000

    fixup aesthetics and colours
    Signed-off-by: Wilfred Mallawa <thulith.mallawa@uqconnect.edu.au>

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,23 @@
 var list = document.getElementById("navMenu-list");
 list.style.maxHeight = "0px";
 
-function shownav() {
+/**
+ * Function to toggle the display height of the navigation menu
+ *  on mobile displays.
+ */
+function showNav() {
     if (list.style.maxHeight == "0px") {
         list.style.maxHeight = "150px"
     } else {
         list.style.maxHeight = "0px"
     }
+}
+
+/**
+ * Function to load in repeated HTML for the navigation menu.
+ */
+function loadNavHTML() {
+  $("#nav-placeholder").load("../nav_menu.html")
 }
 
 /* The following script is implemented for the slide-show on the Home Page
